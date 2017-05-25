bc835c03cbea0e26492025a019ed6859af8851fb
Xiang <zhangxiang1@jd.com>
2017-05-25 15:36:00 +0800

    jade

--- nba/routes/index.js
+++ nba/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+const request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var url = 'http://127.0.0.1:3000/api?func=getVideoList'
+  request(url, function (error, response, body) {
+    if (error) {
+      res.render('error');
+    }else {
+      var result = JSON.parse(body);
+      res.render('index', { videoList: result.data });
+    }
+});
+
 });
 
 module.exports = router;
